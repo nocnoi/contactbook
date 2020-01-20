a62e1cb3813134bed9cf4a53570dfc88a250aa4f
Nisha Salim <nocturne.noire@gmail.com>
2020-01-20 21:21:58 +0530

    add server details to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '52.82.17.7', user: 'deploy', roles: %w{web app db}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
